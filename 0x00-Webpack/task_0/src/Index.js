8c6855ef702f0fb92fa1be7df6799780352a817b
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-07-22 14:29:05 +0100

    Create Index.js

---
+++ 0x00-Webpack/task_0/src/Index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+  $('body').append('<h1>Holberton Dashboard</h1>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
